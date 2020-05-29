0e7498218b1d7b08ef22b8540c31709d28f97595
Gabriel Gohier-Roy <ggohierroy@gmail.com>
2020-05-29 07:26:23 -0400

    missing state for backroom deals and popular partners

--- cityofthebigshoulders.game.php
+++ cityofthebigshoulders.game.php
@@ -3487,7 +3487,7 @@ class CityOfTheBigShoulders extends Table
             case 'popular_partners':
             case 'backroom_deals':
                 $state = $this->gamestate->state();
-                if( $state['name'] != 'playerDistributeGoodsPhase' && $state['name'] != 'playerDividendsPhase' )
+                if( $state['name'] != 'playerDistributeGoodsPhase' && $state['name'] != 'playerDividendsPhase' && $state['name'] != 'playerUseAssetsPhase' )
                     throw new BgaUserException( self::_("Capital Asset can only be used after the production step of the Operation Phase") );
                 // increase share value one step
                 self::increaseShareValue($short_name, 1);
